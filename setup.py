e7fd252dd90a105653b5944b5df2cb15dedfe641
Lucas Willems <lcswillems@gmail.com>
2019-07-25 13:26:55 +0200

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torch_ac",
-    version="1.0.2",
+    version="1.1.0",
     keywords="reinforcement learning, actor-critic, a2c, ppo, multi-processes, gpu",
     packages=find_packages(),
     install_requires=[
